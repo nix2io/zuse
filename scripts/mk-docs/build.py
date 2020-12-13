8fe0531be9386bc627e12a8298d42171990ae66d
Max Koon <maxk@nix2.io>
2020-12-12 20:20:32 -0500

    backup before recursive asset copy

--- scripts/mk-docs/build.py
+++ scripts/mk-docs/build.py
@@ -1,15 +1,16 @@
 from jinja2 import Template, Environment, FileSystemLoader
-from os import path, listdir, mkdir
 from json import loads
 from json_minify import json_minify
 from typing import List
+import shutil
+import os
 
 
 # Some constants
-ROOT_DIR = path.join('..', '..')
-DOCS_DIR = path.join(ROOT_DIR, 'docs/')
-LANG_DIR = path.join(ROOT_DIR, "lang/");
-SPECIFICATION_FILE = path.join(ROOT_DIR, "spec.jsonc");
+ROOT_DIR = os.path.join('..', '..')
+DOCS_DIR = os.path.join(ROOT_DIR, 'docs/')
+LANG_DIR = os.path.join(ROOT_DIR, "lang/")
+SPECIFICATION_FILE = os.path.join(ROOT_DIR, "spec.jsonc")
 JINJA_ENVIRONMENT = Environment(loader=FileSystemLoader("templates/"))
 
 
@@ -33,8 +34,8 @@ def get_group_functions(group_name: str) -> List[str]:
     '''
     Returns a list of functions from a group name
     '''
-    group_folder = path.join(LANG_DIR, group_name + '/')
-    return [name for name in listdir(group_folder) if path.isdir(path.join(group_folder, name))]
+    group_folder = os.path.join(LANG_DIR, group_name + '/')
+    return [name for name in os.listdir(group_folder) if os.path.isdir(os.path.join(group_folder, name))]
 
 def render(template_name: str, *args, **kwargs):
     template_path = 'templates/{}.j2'.format(template_name)
@@ -45,9 +46,21 @@ def render(template_name: str, *args, **kwargs):
 
 def create(template_name: str, target_file: str, *args, **kwargs):
     rendered = render(template_name, *args, **kwargs)
-    with open(path.join(DOCS_DIR, target_file), 'w') as f:
+    with open(os.path.join(DOCS_DIR, target_file), 'w') as f:
         f.write(rendered)
         f.close()
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
+                shutil.copy2(s, d)
 
 # Get the language spec
 language_specification = get_language_specification()
@@ -55,14 +68,26 @@ language_specification = get_language_specification()
 # Create the docs dir
 # mkdir(DOCS_DIR)
 
-groups = language_specification['groups']
-
 groups = []
+for group in language_specification['groups']:
+    # Get this group's functions
+    group_function_names = get_group_functions(group)
+    group_functions = []
+    for func_name in group_function_names:
+        func_dir = os.path.join(LANG_DIR, group + '/', func_name + '/')
+        func_spec_file = os.path.join(func_dir, 'spec.jsonc')
+        func_spec = read_json_file(func_spec_file)
 
+        group_functions.append({ "name": func_name, "description": func_spec[1].get('description', 'Undocumented Function')})
 
+    groups.append({
+        'label': group[0].upper() + group[1:],
+        'functions': group_functions
+    })
 
+# Create the cheat sheet
+create('home', 'index.html')
+create('cheat_sheet', 'cheat_sheet.html', groups=[[group for i, group in enumerate(groups) if i % 2 == 0], [group for i, group in enumerate(groups) if i % 2 != 0]])
 
-
-
-create('index', 'index.html', groups=[[group for i, group in enumerate(groups) if i % 2 == 0], [group for i, group in enumerate(groups) if i % 2 != 0]])
-
+# Copy all the assets
+copytree('assets/', os.path.join(DOCS_DIR, 'assets/'))
